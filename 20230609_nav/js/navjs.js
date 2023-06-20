78424427364625de4716153e2400c6612bad0469
didwldk <118715150+didwldk@users.noreply.github.com>
2023-06-20 10:59:04 +0900

    nav js, style.css -> nav.css

--- 20230609_nav/js/navjs.js
+++ 20230609_nav/js/navjs.js
@@ -9,14 +9,23 @@
 // /nav-list -> navListUl
 // const navToggleDiv = document.getElementById("nav_toggle");
 const navToggleI = nav_toggle.getElementsByTagName("i")[0];
-const navListUl = document.getElementsByClassName("nav-list");
+const navListUl = document.getElementsByClassName("nav-list")[0];
+
 //2. Event Handling
-// 햄버거 메뉴 클릭 -> 햄버거 메뉴 i "bi bi-list" <-> X "bi bi-x-lg"
-// 햄버거 메뉴 클릭 -> .nav-list에 .show-menu 보여주기 <-> 안보여주기
-nav_toggle.onclick = () => alert("밤톨주먹밥과 브로꼬리");//function 제외하며 간략화(람다식) and 중괄호 생략(1개일 때)
+// nav_toggle.onclick = () => alert("밤톨주먹밥과 브로꼬리");//function 제외하며 간략화(람다식) and 중괄호 생략(1개일 때)
 // nav_toggle.onclick = alert("안녕"); // 바로 실행
+
+nav_toggle.onclick = () => {
+    // 햄버거 메뉴 클릭 -> 햄버거 메뉴 i "bi bi-list" <-> X "bi bi-x-lg"
+    navToggleI.classList.toggle("bi-list");
+    navToggleI.classList.toggle("bi-x-lg");
+    // 햄버거 메뉴 클릭 -> .nav-list에 .show-menu 보여주기 <-> 안보여주기
+    navListUl.classList.toggle("show-menu");
+}
+
 //_____________________________________________________________________
 //HTML -> JS
+
 //1. id
 // const navToggleDiv = document.getElementById("nav_toggle");
 // nav_toggle; // JS 변수 규칙은 지켜야 함
