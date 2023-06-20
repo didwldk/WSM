20bec6e13c86e6c653fc1bca05c566e1c949f6d5
didwldk <118715150+didwldk@users.noreply.github.com>
2023-06-20 12:17:53 +0900

    bookshelf.css, bookshelf.j

---
+++ 20230620_bookreviewcalendar/js/bookshelf.js
@@ -0,0 +1,15 @@
+//.book-item > img 태그의 src 속성 초기화 하자
+//HTML -> JS
+const bookItems = document.getElementsByClassName("book-item");
+let i = 0;
+for(let bookItem of bookItems){
+    const bookItemImg = bookItem.getElementsByTagName("img")[0];
+    //.book-item 하나씩 꺼내어 img 태그에 가져오기
+    bookItemImg.src = books[i].img;
+    //data.js에서 img 값 가져와서 img 태그에 적용
+    i++;
+}
+
+// const bookItems = bookItems[0];
+// console.log(bookItems);
+// const book
